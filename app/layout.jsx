99dd740623a4fd1a6e3ec19f868ab754e00e5df6
Diyorbek92 <diyorbek2017u@gmail.com>
2024-02-12 09:01:50 +0500

    nextjs

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+// components
+import Navbar from "./components/Navbar";
+
+const montserrat = Montserrat({
+  subsets: ["latin", "cyrillic"],
+  weight: ["400", "500"],
+});
+
+export const metadata = {
+  title: "Home",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" data-theme="light">
+      <body className={montserrat.className}>
+        <Navbar />
+        <main className="max-w-4xl mx-auto">{children}</main>
+      </body>
+    </html>
+  );
+}
